542dc77f2a32aa2eb684421c2374113f4d59c422
= <=>
2023-09-25 12:06:26 +0700

    week4

---
+++ week-04/src/praktikum5.dart
@@ -0,0 +1,14 @@
+void main() {
+  // var record = ('first', a: 2, b: true, 'last');
+  // print(record);
+
+  // (String, int) mahasiswa = ('Jenio Dwi Setyo Favian Gian', 2141720212);
+  // print(mahasiswa);
+
+  var mahasiswa2 = ('first', a: 2, b: true, 'last');
+
+  print(mahasiswa2.$1); 
+  print(mahasiswa2.a); 
+  print(mahasiswa2.b); 
+  print(mahasiswa2.$2); 
+}
