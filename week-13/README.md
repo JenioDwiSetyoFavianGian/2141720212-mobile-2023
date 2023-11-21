bfb3c4e9c321ae5f46d9d18d46c5f7a125b18750
= <=>
2023-11-22 01:07:11 +0700

    W13: Jawaban Soal 6

--- week-13/README.md
+++ week-13/README.md
@@ -11,3 +11,15 @@
 ## Penjelasan Soal 5 Langkah 9
 
 list digunakan untuk menyimpan dan memanipulasi data secara sinkron, sementara await for digunakan dalam asyncio untuk iterasi asinkron melalui objek iterable dalam fungsi asinkron, memungkinkan penanganan operasi yang memerlukan waktu.
+
+---
+
+## Hasil Praktikum_2
+
+![Screenshot](docs/praktikum2.gif)
+
+---
+
+Langkah8: untuk menginisialisasi stream controller dan sink-nya. stream kemudian diambil dari controller dengan menggunakan numberStreamController.stream. Kemudian, listen digunakan pada stream untuk mendengarkan perubahan yang terjadi pada Stream. Ketika ada perubahan (saat angka baru ditambahkan ke dalam Stream), fungsi yang diberikan akan dipanggil. Di sini, nilai yang diterima dari Stream akan diperbarui dalam state dengan menggunakan setState, dan nilai terakhir lastNumber akan diperbarui dengan nilai dari Stream.
+
+Langkah10: addRandomNumber() method yang bertujuan untuk menambahkan angka acak ke dalam Stream. Pertama, sebuah objek dari kelas Random diciptakan untuk menghasilkan angka acak. Kemudian, angka acak di-generate menggunakan nextInt(10) untuk mendapatkan angka random dari 0 hingga 9. Terakhir, angka tersebut ditambahkan ke dalam Sink yang ada di numberStream melalui method addNumberToSink().
--- week-13/src/lib/main.dart
+++ week-13/src/lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+
 import 'stream.dart';
 
 void main() {
@@ -31,27 +35,162 @@ class StreamHomePage extends StatefulWidget {
 class _StreamHomePageState extends State<StreamHomePage> {
   Color bgColor = Colors.blueGrey;
   late ColorStream colorStream;
+  int lastNumber = 0;
+  late StreamController numberSteamController;
+  late NumberStream numberStream;
+
   @override
   Widget build(BuildContext context) {
-         return Scaffold(
-      appBar: AppBar(
-        title: const Text('Vian Stream'),
-      ),
-      body: Container(
-        decoration: BoxDecoration(color: bgColor),
-         ));
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Stream Vian'),
+        ),
+        body: SizedBox(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(lastNumber.toString()),
+              ElevatedButton(
+                onPressed: () => addRandomNumber(),
+                child: const Text('New Random Number'),
+              )
+            ],
+          ),
+        ));
   }
-void changeColor() async {
-  colorStream.getColors().listen((eventColor) {
-    setState(() {
-      bgColor = eventColor;
+
+  @override
+  void initState() {
+    super.initState();
+
+    numberStream = NumberStream();
+    numberSteamController = numberStream.controller;
+    Stream stream = numberSteamController.stream;
+    stream.listen((event) {
+      setState(() {
+        lastNumber = event;
+      });
     });
-  });
+  }
+
+  void changeColor() async {
+    // await for (var eventColor in colorStream.getColors()) {
+    //   setState(() {
+    //     bgColor = eventColor;
+    //   });
+    // }
+
+    colorStream.getColors().listen((event) {
+      setState(() {
+        bgColor = event;
+      });
+    });
+  }
+
+  void addRandomNumber() {
+    Random random = Random();
+    int myNum = random.nextInt(10);
+    numberStream.addNumberToSink(myNum);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    numberSteamController.close();
+  }
 }
-@override
-void initState() {
-  super.initState();
-  colorStream = ColorStream();
-  changeColor();
-}
-}
+// import 'dart:async';
+// import 'dart:math';
+
+// import 'package:flutter/material.dart';
+
+// import 'stream.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Vian Stream',
+//       theme: ThemeData(
+//         primarySwatch: Colors.grey,
+//       ),
+//       home: const StreamHomePage(),
+//     );
+//   }
+// }
+
+// class StreamHomePage extends StatefulWidget {
+//   const StreamHomePage({super.key});
+
+//   @override
+//   State<StreamHomePage> createState() => _StreamHomePageState();
+// }
+
+// class _StreamHomePageState extends State<StreamHomePage> {
+//   Color bgColor = Colors.blueGrey;
+//   late ColorStream colorStream;
+//   int lastNumber = 0;
+//   late StreamController numberStreamController;
+//   late NumberStream numberStream;
+//   @override
+//   Widget build(BuildContext context) {
+//          return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Vian Stream'),
+//       ),
+//       body: SizedBox(
+//         width: double.infinity,
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//           crossAxisAlignment: CrossAxisAlignment.center,
+//           children: [
+//             Text(lastNumber.toString()),
+//             ElevatedButton(
+//               onPressed: () => addRandomNumber(),
+//               child: Text('New Random Number'),
+//             )
+//           ],
+//         ),
+//       ));
+//   }
+// // void changeColor() async {
+// //   colorStream.getColors().listen((eventColor) {
+// //     setState(() {
+// //       bgColor = eventColor;
+// //     });
+// //   });
+// // }
+// void dispose() {
+//   numberStreamController.close();
+//   super.dispose();
+// }
+// @override
+//   void initState() {
+//     super.initState();
+
+//     numberStream = NumberStream();
+//     numberStreamController = numberStream.controller;
+//     Stream stream = numberStreamController.stream;
+//     stream.listen((event) {
+//       setState(() {
+//         lastNumber = event;
+//       });
+//     });
+//   }
+// }
+// void addRandomNumber() {
+//     Random random = Random();
+//     int myNum = random.nextInt(10);
+//     NumberStream.addNumberToSink(myNum);
+//   }
+
